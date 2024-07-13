ebb97dc645b226f34a65f881a40a71114c53f7ac
lizzA-007 <kenoras42@gmail.com>
2024-07-13 10:51:44 +0000

    lizzycoin

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "lizzy"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "liz"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "lizzycoin"
 )
 
 var ID ids.ID
